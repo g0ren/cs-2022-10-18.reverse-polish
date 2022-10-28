c1f6161ceeb2ddb5fe42ebf2d8c0dfc5e380fcc3
g0ren <g0ren@mail.ru>
2022-10-28 16:33:01 +0300

    Add project files.

---
+++ cs-reverse-polish/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+
+delegate int Operator(Stack<int> s);
+
+class Program
+{
+
+    public static Dictionary<string, Operator> Operators = new Dictionary<string, Operator>()
+    {
+        {"+", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return left + right;
+        })},
+        {"-", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return left - right;
+        })},
+        {"*", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return left * right;
+        })},
+        {"/", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return left / right;
+        })},
+        {"%", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return left % right;
+        })},
+        {"**", new Operator((Stack<int> s) =>
+        {
+        var right = s.Pop();
+        var left = s.Pop();
+        return (int)Math.Pow(left, right);
+        })},
+        {
+            "neg", new Operator((Stack<int> s) => 
+            -1 * s.Pop())
+        }
+    };
+
+    public static void Main(string[] args)
+    {
+
+        var s = new Stack<int>();
+        string[] input = (string.Empty + Console.ReadLine()).Split(' ');
+        foreach(string e in input)
+        {
+            if (Operators.ContainsKey(e))
+            {
+                s.Push(Operators[e](s));
+            }
+            else
+            {
+                s.Push(Convert.ToInt32(e));
+            }
+        }
+        Console.WriteLine(s.Peek().ToString());
+    }
+}
